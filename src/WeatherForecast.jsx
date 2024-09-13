4c2bbe22deb0263ddc531de58bb34662469d6804
seldesoky <sameh.s.eldesoky@gmail.com>
2024-09-12 23:05:37 -0400

    Final commit

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -1,14 +1,14 @@
 import './WeatherForecast.css';
+import WeatherIcon from './WeatherIcon';
+import WeatherData from './WeatherData';
 
 const WeatherForecast = ({ day, icon, iconAlt, conditions, time }) => {
   return (
     <div className="weather">
-      <h2>{day}</h2>
-      <img src={icon} alt={iconAlt} />
-      <p><span>Conditions: </span>{conditions}</p>
-      <p><span>Time: </span>{time}</p>
+      <WeatherData day={day} conditions={conditions} time={time} />
+      <WeatherIcon icon={icon} iconAlt={iconAlt} />
     </div>
   );
-}
+};
 
 export default WeatherForecast;
---
+++ src/WeatherIcon.jsx
@@ -0,0 +1,6 @@
+const WeatherIcon = ({ icon, iconAlt }) => {
+    return <img src={icon} alt={iconAlt} />;
+  };
+  
+  export default WeatherIcon;
+  
